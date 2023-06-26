3731dcb2d6c8adf4552a137c792d40e3bae54c5b
antonin caudron <antonin@area17.com>
2023-06-26 17:02:04 +0200

    Adjust icon component loading
    Try to load first icon from application and fallback on package icons
    Icon is not rendering if view doesn't exist

--- src/Components/Icon.php
+++ src/Components/Icon.php
@@ -20,21 +20,38 @@ class Icon extends VitrineComponent
         $name = null,
         $ariaLabel = null,
         $iconPath = null,
+        $ui = []
     )
     {
         $this->name = $name;
         $this->ariaLabel = $ariaLabel;
         $this->iconComponent = $iconPath ?? $this->getIconPath();
+
+        parent::__construct($ui);
+    }
+
+    public function shouldRender()
+    {
+        return $this->iconComponent;
     }
 
     public function render(): View
     {
-        return view('vitrine-ui::components.icon.index');
+        return view($this->iconComponent);
     }
 
-    public function getIconPath()
+    public function getIconPath(): bool|string
     {
-        $iconPath = 'icon._icons.'. $this->name;
-        return view()->exists($iconPath) ? $iconPath : 'vitrine-ui::'.$iconPath;
+        $iconPath = config('vitrine-ui.icons_view_path', 'icons'). $this->name;
+
+        $viewExists = view()->exists($iconPath);
+        if($viewExists) return $iconPath;
+
+        $localPath = 'vitrine-ui::components.icon._icons.'.$this->name;
+        $viewExists = view()->exists($localPath);
+
+        if($viewExists) return $localPath;
+
+        return false;
     }
 }
