cf07bb6804b5cd596926c1c3eadd13904d565158
antonin caudron <antonin@area17.com>
2023-06-29 18:33:28 +0200

    Add more allowed keys for breadcrumbs components
    Allow url for href key
    Allow label key for text key

--- resources/views/components/breadcrumbs/breadcrumbs.blade.php
+++ resources/views/components/breadcrumbs/breadcrumbs.blade.php
@@ -1,19 +1,19 @@
 @if(filled($items))
     <{{ $tag ?? 'nav' }} aria-label="breadcrumbs" {{ $attributes->class($ui('breadcrumbs')) }}>
         @foreach ($items as $item)
-            @if ($item['href'] ?? null)
+            @if ($item['href'] ??  $item['url'] ?? null)
                 <a
                     class="{{ $ui('breadcrumbs', ['item', 'link']) }}"
-                    href="{{$item['href']}}"
+                    href="{{$item['href'] ?? $item['url']}}"
                     @if($loop->last)  aria-current="page"  @endif
                 >
-                    {{ $item['text'] ?? '' }}
+                    {{ $item['text'] ?? $item['label'] ?? '' }}
                 </a>
             @else
                 <span class="{{ $ui('breadcrumbs', 'item') }}"
                       @if($loop->last)  aria-current="page"  @endif
                 >
-                    {{ $item['text'] ?? '' }}
+                    {{ $item['text'] ?? $item['label'] ?? '' }}
                 </span>
             @endif
         @endforeach
