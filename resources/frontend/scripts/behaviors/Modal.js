4b5722e79761e4943fd3c9b1fb2c79eb65dfb520
antonin caudron <antonin@area17.com>
2023-07-26 11:56:08 +0200

    Add option to open modal from js event

--- resources/frontend/scripts/behaviors/Modal.js
+++ resources/frontend/scripts/behaviors/Modal.js
@@ -28,9 +28,16 @@ const Modal = createBehavior(
 
                 this.$node.dispatchEvent(new CustomEvent('Modal:closed'))
                 document.dispatchEvent(new CustomEvent('Modal:hasClosed'))
+                this.disposeOpenEvents()
             }
         },
-
+        openFromOutside(e) {
+            if(e.detail?.modalId === this.$modalId && !this._data.isActive) {
+                e.preventDefault()
+                e.stopPropagation()
+                this.open()
+            }
+        },
         open() {
             document.dispatchEvent(new CustomEvent('Modal:closeAll'))
             document.dispatchEvent(new CustomEvent('Modal:hasOpened'))
@@ -43,9 +50,11 @@ const Modal = createBehavior(
             this.$node.setAttribute('data-active', 'true')
             this._data.isActive = true
 
+            this.registerOpenEvents()
+
             setTimeout(() => {
                 this._data.focusTrap.activate()
-            }, 500)
+            }, 600)
         },
 
         handleEsc(e) {
@@ -62,52 +71,8 @@ const Modal = createBehavior(
 
         handleCloseInside(e) {
             e.stopPropagation()
-        }
-    },
-    {
-        init() {
-            this.$focusTrap = this.getChild('focus-trap')
-            this.$closeButtons = this.getChildren('close-trigger')
-            this.$initialFocus = this.getChild('initial-focus')
-            this.$clickOutside = this.options['panel'] || this.options['clickoutside']
-
-            if (!this.$initialFocus) {
-                console.warn(
-                    'No initial focus element found. Add a `h1` with the attribute `data-Modal-initial-focus`. The `h1` should also have an id that matches the modal id with `_title` appended'
-                )
-            }
-
-            this._data.focusTrap = focusTrap.createFocusTrap(this.$focusTrap, {
-                initialFocus: this.$initialFocus,
-                clickOutsideDeactivates: this.$clickOutside
-            })
-
-            this._data.isActive = false
-
-            if (this.$closeButtons) {
-                this.$closeButtons.forEach((closeButton) => {
-                    closeButton.addEventListener('click', this.close, false)
-                })
-            }
-
-            this.$node.addEventListener('Modal:toggle', this.toggle, false)
-            this.$node.addEventListener('Modal:open', this.open, false)
-            this.$node.addEventListener('Modal:close', this.close, false)
-
-            document.addEventListener('Modal:closeAll', this.close, false)
-            document.addEventListener('keyup', this.handleEsc, false)
-
-            // add listener to modal toggle buttons
-            const modalId = this.$node.getAttribute('id')
-
-            this.$triggers = document.querySelectorAll(
-                `[data-modal-target="#${modalId}"]`
-            )
-
-            this.$triggers.forEach((trigger) => {
-                trigger.addEventListener('click', this.toggle, false)
-            })
-
+        },
+        registerOpenEvents() {
             if (this.$clickOutside) {
                 this.$focusTrap.addEventListener(
                     'click',
@@ -126,28 +91,8 @@ const Modal = createBehavior(
                 )
             }
         },
-        destroy() {
-            this.close()
-
-            if (this.$closeButtons) {
-                this.$closeButtons.forEach((closeButton) => {
-                    closeButton.removeEventListener('click', this.close)
-                })
-            }
-
-            this.$node.removeEventListener('Modal:toggle', this.toggle)
-            this.$node.removeEventListener('Modal:open', this.open)
-            this.$node.removeEventListener('Modal:close', this.close)
-            this.$node.removeEventListener('click', this.handleClickOutside)
-
-            document.removeEventListener('Modal:closeAll', this.close)
-            document.removeEventListener('keyup', this.handleEsc)
-
-            this.$triggers.forEach((trigger) => {
-                trigger.removeEventListener('click', this.toggle)
-            })
-
-            if (this.options['panel']) {
+        disposeOpenEvents() {
+            if (this.$clickOutside) {
                 this.$focusTrap.removeEventListener(
                     'click',
                     this.handleCloseInside,
@@ -164,6 +109,78 @@ const Modal = createBehavior(
                     false
                 )
             }
+        }
+    },
+    {
+        init() {
+            this.$focusTrap = this.getChild('focus-trap')
+            this.$closeButtons = this.getChildren('close-trigger')
+            this.$initialFocus = this.getChild('initial-focus')
+            this.$clickOutside = this.options['panel'] || this.options['clickoutside']
+
+            if (!this.$initialFocus) {
+                console.warn(
+                    'No initial focus element found. Add a `h1` with the attribute `data-Modal-initial-focus`. The `h1` should also have an id that matches the modal id with `_title` appended'
+                )
+            }
+
+            this._data.focusTrap = focusTrap.createFocusTrap(this.$focusTrap, {
+                initialFocus: this.$initialFocus,
+                fallbackFocus: this.$node,
+                clickOutsideDeactivates: this.$clickOutside
+            })
+
+            this._data.isActive = false
+
+            if (this.$closeButtons) {
+                this.$closeButtons.forEach((closeButton) => {
+                    closeButton.addEventListener('click', this.close, false)
+                })
+            }
+
+            this.$node.addEventListener('Modal:open', this.open, false)
+            this.$node.addEventListener('Modal:toggle', this.toggle, false)
+            this.$node.addEventListener('Modal:close', this.close, false)
+
+            document.addEventListener('Modal:openById', this.openFromOutside, false)
+            document.addEventListener('Modal:closeAll', this.close, false)
+            document.addEventListener('keyup', this.handleEsc, false)
+
+            // add listener to modal toggle buttons
+            this.$modalId = this.$node.getAttribute('id')
+
+            this.$triggers = document.querySelectorAll(
+                `[data-modal-target="#${this.$modalId}"]`
+            )
+
+            this.$triggers?.forEach((trigger) => {
+                trigger.addEventListener('click', this.toggle, false)
+            })
+
+        },
+        destroy() {
+            this.close()
+
+            if (this.$closeButtons) {
+                this.$closeButtons.forEach((closeButton) => {
+                    closeButton.removeEventListener('click', this.close)
+                })
+            }
+
+            this.$node.removeEventListener('Modal:open', this.open)
+            this.$node.removeEventListener('Modal:toggle', this.toggle)
+            this.$node.removeEventListener('Modal:close', this.close)
+            this.$node.removeEventListener('click', this.handleClickOutside)
+
+            document.removeEventListener('Modal:openById', this.openFromOutside)
+            document.removeEventListener('Modal:closeAll', this.close)
+            document.removeEventListener('keyup', this.handleEsc)
+
+            this.$triggers?.forEach((trigger) => {
+                trigger.removeEventListener('click', this.toggle)
+            })
+
+            this.disposeOpenEvents()
         }
     }
 )
