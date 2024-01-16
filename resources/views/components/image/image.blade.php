6d8fa96c7d5bdf75a561abf8c28e437d9491e4d2
Antoine Doury <antoine@area17.com>
2024-01-16 10:32:26 +0100

    Image render - reorder attributes

--- resources/views/components/image/image.blade.php
+++ resources/views/components/image/image.blade.php
@@ -7,13 +7,13 @@
     @break
     @case('static' || 'twill-image-array')
         <img {{ $attributes->class([$ui('media', 'image'), $imageOptions['class'] ?? null]) }}
+                @if(Arr::has($imageOptions, 'attributes') && is_array($imageOptions['attributes'])) {!! $setAttributes($imageOptions['attributes'])!!} @endif
                 @if(Arr::has($imageOptions, 'width')) width="{{ $imageOptions['width'] }}" @endif
                 @if(Arr::has($imageOptions, 'height')) height="{{ $imageOptions['height'] }}" @endif
                 @if(Arr::has($imageOptions, 'loading')) loading="{{ $imageOptions['loading'] }}" @endif
                 @if(Arr::has($imageOptions, 'sizes')) sizes="{{ $imageOptions['sizes'] }}" @endif
-                @if(Arr::has($imageOptions, 'attributes') && is_array($imageOptions['attributes'])) {!! $setAttributes($imageOptions['attributes'])!!} @endif
                 @if(Arr::has($image, 'srcset')) srcset="{{ $image['srcset'] }}" @endif
-                @if (Arr::has($image, 'srcSet')) srcset="{{ $image['srcSet'] }}" @endif
+                @if(Arr::has($image, 'srcSet')) srcset="{{ $image['srcSet'] }}" @endif
                 src="{{ $image['src'] }}"
                 alt="{{ $image['alt'] ?? '' }}" />
     @break
