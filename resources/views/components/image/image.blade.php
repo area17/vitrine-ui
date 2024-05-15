deeb94cfd239c3c699a7c86c02400ac76dc5fe35
Antoine Doury <antoine@area17.com>
2024-05-15 16:58:46 +0200

    Image component : update how class are setup for static image

--- resources/views/components/image/image.blade.php
+++ resources/views/components/image/image.blade.php
@@ -6,7 +6,7 @@
         {!!  A17\Twill\Image\Models\StaticImage::makeFromSrc($staticSettings)->render($imageOptions) !!}
     @break
     @case('static' || 'twill-image-array')
-        <img {{ $attributes->class([$ui('media', 'image'), $imageOptions['class'] ?? null]) }}
+        <img {{ $attributes->class([$ui('media', 'image'), $imageOptions['class'] ?? $imageOptions['imageClass'] ?? null]) }}
                 @if(Arr::has($imageOptions, 'attributes') && is_array($imageOptions['attributes'])) {!! $setAttributes($imageOptions['attributes'])!!} @endif
                 @if(Arr::has($imageOptions, 'width')) width="{{ $imageOptions['width'] }}" @endif
                 @if(Arr::has($imageOptions, 'height')) height="{{ $imageOptions['height'] }}" @endif
