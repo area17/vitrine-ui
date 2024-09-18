c0fb8e169ec45df1fffee15a0bece27677d22f65
Antoine Doury <antoine@area17.com>
2024-09-18 16:36:03 +0200

    Update Input Component to handle right spacing settings from the theming file

--- resources/views/components/form/input/input.blade.php
+++ resources/views/components/form/input/input.blade.php
@@ -1,5 +1,5 @@
 <input type="{{$type ?? 'text'}}"
-    {{ $attributes->class([$ui('input', 'input'), 'pr-40' => $withIconRight ?? false]) }}
+    {{ $attributes->class([$ui('input', 'input'), $ui('input', 'input-icon-right') => $withIconRight ?? false]) }}
     @if($id || $name) id="{{$id ? $id : $name}}" @endif
     @if($name) name="{{$name}}" @endif
     @if($value) value="{{$value}}" @endif
