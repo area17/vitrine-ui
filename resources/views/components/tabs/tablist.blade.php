4f8f01e5c8159d4b0d0c40486a006f4d08f7b5be
Florrie <florrie@area17.com>
2024-10-04 17:09:38 -0400

    Change tablist to list element

--- resources/views/components/tabs/tablist.blade.php
+++ resources/views/components/tabs/tablist.blade.php
@@ -1,17 +1,19 @@
-<div {{ $attributes->class([$ui('tab-list', 'base')]) }}
-     @if($tabListId) aria-labelledby="{{ $tabListId }}"
-     @elseif($ariaLabel) aria-label="{{$ariaLabel}}"
-        @endif>
+<ul role="tablist"
+    {{ $attributes->class([$ui('tab-list', 'base')]) }}
+    @if ($tabListId) aria-labelledby="{{ $tabListId }}"
+     @elseif($ariaLabel) aria-label="{{ $ariaLabel }}" @endif>
 
-     @foreach($tabsNames as $tabName)
-          <x-vui-button id="{{ $name.'-'.$loop->index }}"
-                        data-Tabs-button=""
-                        aria-controls="{{  $name.'-panel-'.$loop->index }}"
-                        role="tab"
-                        aria-selected="{{ $loop->index === $selectedIndex ? 'true' : 'false' }}"
-                        :class="$ui('tab-list', 'button')"
-                        :variant="$tabButtonVariant">
-               {{ $tabName }}
-          </x-vui-button>
-     @endforeach
-</div>
+    @foreach ($tabsNames as $tabName)
+        <li @if (count($tabsNames) == 1) role="presentation" @endif>
+            <x-vui-button id="{{ $name . '-' . $loop->index }}"
+                          data-Tabs-button=""
+                          role="tab"
+                          aria-controls="{{ $name . '-panel-' . $loop->index }}"
+                          aria-selected="{{ $loop->index === $selectedIndex ? 'true' : 'false' }}"
+                          :class="$ui('tab-list', 'button')"
+                          :variant="$tabButtonVariant">
+                {{ $tabName }}
+            </x-vui-button>
+        </li>
+    @endforeach
+</ul>
