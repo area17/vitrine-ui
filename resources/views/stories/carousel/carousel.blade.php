4a9c0638efca5075ed0cf41b316b78bb7f2df70a
Antoine Doury <antoine@area17.com>
2025-01-06 12:09:20 +0100

    Carousel - update stories

--- resources/views/stories/carousel/carousel.blade.php
+++ resources/views/stories/carousel/carousel.blade.php
@@ -1,14 +1,53 @@
 @storybook([
     'status' => 'readyForQA',
-    'preset' => 'button.base',
+    'args' => [
+        'items' => [
+            [
+                'src' => 'https://placehold.co/600x400.png',
+                'alt' => 'Sample Alt Text',
+            ],
+            [
+                'src' => 'https://placehold.co/600x400.png',
+                'alt' => 'Sample Alt Text',
+            ],
+            [
+                'src' => 'https://placehold.co/600x400.png',
+                'alt' => 'Sample Alt Text',
+            ],
+            [
+                'src' => 'https://placehold.co/600x400.png',
+                'alt' => 'Sample Alt Text',
+            ],
+            [
+                'src' => 'https://placehold.co/600x400.png',
+                'alt' => 'Sample Alt Text',
+            ],
+        ],
+    ],
 ])
 
-<x-vui-button :href="$href ?? null"
-              :icon="$icon ?? null"
-              :icon-position="$iconPosition ?? null"
-              :static="$static ?? null"
-              :disabled="$disabled ?? null"
-              :active="$active ?? null"
-              variant="primary">
-    {{ $label ?? null }}
-</x-vui-button>
+@php
+    $MEDIA_CAROUSEL = [
+        'slidesPerView' => 'auto',
+        'freeMode' => false,
+        'allowTouchMove' => true,
+        'loop' => false,
+        'spaceBetween' => 10,
+    ];
+@endphp
+
+<script>
+    window.A17 = window.A17 || {};
+    window.A17.sliderConfigurations = {
+        'media-carousel': {
+            ...@json($MEDIA_CAROUSEL)
+        }
+    };
+</script>
+
+<x-vui-carousel @class(['h-[320px] px-outer-gutter'])
+                :items="$items"
+                component="x-vui-image"
+                configuration="media-carousel"
+                :with-controls="false"
+                item-class="flex" />
