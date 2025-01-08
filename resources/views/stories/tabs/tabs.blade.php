0ba2b4fa8124e011985ecf5d73ea96da16807df7
Antoine Doury <antoine@area17.com>
2025-01-08 13:03:47 +0100

    Stories - set default status for tabs

--- resources/views/stories/tabs/tabs.blade.php
+++ resources/views/stories/tabs/tabs.blade.php
@@ -3,7 +3,7 @@
     'layout' => 'fullscreen',
     'args' => [
         'title' => 'Tabs',
-        'name' => 'tab',
+        'name' => 'faq',
         'tabsNames' => ['Tab 1', 'Tab 2', 'Tab 3'],
         'titleLevel' => 3,
     ],
@@ -13,17 +13,21 @@
             :name="$name"
             :tabs-names="$tabsNames"
             :title-level="$titleLevel">
-    <div id="tab-panel-0">
+    <div id="tab_faq-panel-0">
         <p>Content 1: Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo autem cum voluptatibus
             exercitationem ea explicabo eum deleniti repudiandae alias delectus nam minima, vel totam consectetur
             officiis ex! Reprehenderit, sunt accusamus.</p>
     </div>
-    <div id="tab-panel-1">
+    <div id="tab_faq-panel-1"
+         hidden
+         inert>
         <p>Content 2: Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo autem cum voluptatibus
             exercitationem ea explicabo eum deleniti repudiandae alias delectus nam minima, vel totam consectetur
             officiis ex! Reprehenderit, sunt accusamus.</p>
     </div>
-    <div id="tab-panel-2">
+    <div id="tab_faq-panel-2"
+         hidden
+         inert>
         <p>Content 3: Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo autem cum voluptatibus
             exercitationem ea explicabo eum deleniti repudiandae alias delectus nam minima, vel totam consectetur
             officiis ex! Reprehenderit, sunt accusamus.</p>
