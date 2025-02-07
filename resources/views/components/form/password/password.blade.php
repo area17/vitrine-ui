776a91c0c6397f71ad6dada922263d10d7377d94
Antoine Doury <antoine@area17.com>
2025-02-07 12:37:39 +0100

    Icon - dont duplicate aria-hidden attribute

--- resources/views/components/form/password/password.blade.php
+++ resources/views/components/form/password/password.blade.php
@@ -25,13 +25,11 @@
             data-PasswordInput-toggle>
         <x-vui-icon class="pointer-events-none inline-block text-[#basa55]"
                     name="eye-hide-24"
-                    data-PasswordInput-iconhidden
-                    aria-hidden="true" />
+                    data-PasswordInput-iconhidden />
         <x-vui-icon class="pointer-events-none inline-block text-[#basa55]"
                     name="eye-24"
                     data-PasswordInput-iconshown
-                    aria-hidden="true"
-                    style="display: none;" />
+                    aria-hidden="true" />
         <span class="sr-only">{{ __('vitrine-ui::fe.form.toggle_password_visibility') }}</span>
     </button>
 </x-vui-form-field>
