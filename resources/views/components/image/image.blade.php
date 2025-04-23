3d3f17af0e74dff8d14fccf0bdbce1f5de68a05e
Antoine Doury <antoine@area17.com>
2025-04-23 15:04:57 +0200

    Image make sure images are being rendered when not using the image object

--- resources/views/components/image/image.blade.php
+++ resources/views/components/image/image.blade.php
@@ -1,13 +1,19 @@
 @if (count($sources ?? []))
     <x-vui-picture {{ $attributes->twMerge([$ui('media', 'image')]) }}
                    :sources="$sources"
+                   :src="$src ?? null"
+                   :alt="$alt ?? null"
+                   :width="$width ?? null"
+                   :height="$height ?? null"
                    :fallBackImg="$setPictureFallbackImg($image)"
                    :loading="$loading" />
 @else
     <x-vui-img {{ $attributes->twMerge([$ui('media', 'image')]) }}
                :img="$image"
+               :src="$src ?? null"
+               :alt="$alt ?? null"
                :loading="$loading"
-               :width="$width"
-               :height="$height"
+               :width="$width ?? null"
+               :height="$height ?? null"
                :sizes="$sizes" />
 @endif
