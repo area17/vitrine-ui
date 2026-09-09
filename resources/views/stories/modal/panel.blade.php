aecf02f5e53981b974583d8beafa0e6438671a4e
Antoine Doury <antoine@area17.com>
2026-09-09 18:19:38 +0200

    Modal stories - Update stories

--- resources/views/stories/modal/panel.blade.php
+++ resources/views/stories/modal/panel.blade.php
@@ -7,7 +7,7 @@
 
 <x-vui-modal id="modalDemo"
              :panel="true">
-    <div class="container">
+    <div class="wysiwyg">
         <h1 data-Modal-initial-focus
             tabindex="-1">Modal Demo Component</h1>
 
@@ -19,8 +19,16 @@
             <button>Focusable Element 1</button>
         </div>
 
+        <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec sed odio dui. Nulla vitae elit libero, a
+            pharetra augue. Maecenas sed diam eget risus varius blandit sit amet non magna. Maecenas sed diam eget risus
+            varius blandit sit amet non magna. Etiam porta sem malesuada magna mollis euismod.</p>
+
         <div class="mt-12">
             <button>Focusable Element 2</button>
         </div>
+
+        <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec sed odio dui. Nulla vitae elit libero, a
+            pharetra augue. Maecenas sed diam eget risus varius blandit sit amet non magna. Maecenas sed diam eget risus
+            varius blandit sit amet non magna. Etiam porta sem malesuada magna mollis euismod.</p>
     </div>
 </x-vui-modal>
