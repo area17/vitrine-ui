0239d675b5bd198eb08beae4a54558099cfaf1e2
Antoine Doury <antoine@area17.com>
2026-09-10 14:59:00 +0200

    Modal - update default styles

--- resources/views/stories/modal/fullscreen.blade.php
+++ resources/views/stories/modal/fullscreen.blade.php
@@ -6,28 +6,31 @@
 </x-vui-button>
 
 <x-vui-modal id="modalDemo">
-    <div class="wysiwyg container">
-        <h1 data-Modal-initial-focus
-            tabindex="-1">Modal Demo Component</h1>
+    <div class="{{ VitrineUI::ui('modal', 'scroller') }}"
+         data-modal-scroller>
+        <div class="wysiwyg">
+            <h1 data-Modal-initial-focus
+                tabindex="-1">Modal Demo Component</h1>
 
-        <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec sed odio dui. Nulla vitae elit libero, a
-            pharetra augue. Maecenas sed diam eget risus varius blandit sit amet non magna. Maecenas sed diam eget risus
-            varius blandit sit amet non magna. Etiam porta sem malesuada magna mollis euismod.</p>
+            <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec sed odio dui. Nulla vitae elit
+                libero, a pharetra augue. Maecenas sed diam eget risus varius blandit sit amet non magna. Maecenas sed
+                diam eget risus varius blandit sit amet non magna. Etiam porta sem malesuada magna mollis euismod.</p>
 
-        <div class="mt-12">
-            <button>Focusable Element 1</button>
+            <div class="mt-12">
+                <button>Focusable Element 1</button>
+            </div>
+
+            <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec sed odio dui. Nulla vitae elit
+                libero, a pharetra augue. Maecenas sed diam eget risus varius blandit sit amet non magna. Maecenas sed
+                diam eget risus varius blandit sit amet non magna. Etiam porta sem malesuada magna mollis euismod.</p>
+
+            <div class="mt-12">
+                <button>Focusable Element 2</button>
+            </div>
+
+            <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec sed odio dui. Nulla vitae elit
+                libero, a pharetra augue. Maecenas sed diam eget risus varius blandit sit amet non magna. Maecenas sed
+                diam eget risus varius blandit sit amet non magna. Etiam porta sem malesuada magna mollis euismod.</p>
         </div>
-
-        <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec sed odio dui. Nulla vitae elit libero, a
-            pharetra augue. Maecenas sed diam eget risus varius blandit sit amet non magna. Maecenas sed diam eget risus
-            varius blandit sit amet non magna. Etiam porta sem malesuada magna mollis euismod.</p>
-
-        <div class="mt-12">
-            <button>Focusable Element 2</button>
-        </div>
-
-        <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec sed odio dui. Nulla vitae elit libero, a
-            pharetra augue. Maecenas sed diam eget risus varius blandit sit amet non magna. Maecenas sed diam eget risus
-            varius blandit sit amet non magna. Etiam porta sem malesuada magna mollis euismod.</p>
     </div>
 </x-vui-modal>
